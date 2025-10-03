2762fdb3dc1e657ca7ec6296e766f0dfc893133f
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2025-10-03 14:03:32 +0530

    feat(codebase): a little refactor and add basics of OS to the flow

---
+++ Backend_fundamentals/OS_fundamentals/00.OSandKernel.js
@@ -0,0 +1,7 @@
+// Operating System (OS):
+// The operating system is the main software that acts as a bridge between hardware and applications. It manages hardware resources (CPU, memory, storage, devices) and makes sure programs run smoothly, securely, and efficiently.
+// A few examples are - Windows OS, macOS and LinuxOS.
+
+// Kernel:
+// The kernel is the core part of the OS that directly talks to the hardware and controls things like process execution, memory, and device management.
+// A few examples are - Linux kernel (used in Ubuntu, Fedora, Android, etc.), BSD kernels (FreeBSD, OpenBSD, NetBSD) and Older UNIX kernels
---
+++ Backend_fundamentals/OS_fundamentals/01.ProcessesInSystem.js
@@ -0,0 +1,11 @@
+//Processes in Systems
+
+//Process in systems can be simply defined as unit of execution which primarily makes use of required memory space, computational power and processing infra to execute a certain program/set of instructions.
+
+//In nodejs we can see the process by simply writing a few lines as this -
+
+const nodeProcess = process;
+console.log(nodeProcess);
+
+//this will give us the details like process version, pid, ppid(parent process ID) etc.
+//the process related details can also be viewed on TaskManager (incase of windows OS) ana ActivityMonitor (in case of macOS).
